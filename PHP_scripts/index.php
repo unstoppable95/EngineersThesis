b7df801a0da6cf6f9e4e6640bbf4b08dfe57c693
Katarzyna Jóźwiak <kasjozw@gmail.com>
2018-08-25 12:54:21 +0200

    add: css style for index.php

--- PHP_scripts/index.php
+++ PHP_scripts/index.php
@@ -12,15 +12,26 @@
 <html>
 <head>
 <title>PANEL LOGOWANIA</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="index_style.css" title="Arkusz stylów CSS">
+
+
+
+
 </head>
 <body>
 
 	<form action="login.php" method="post">
-	
-		Login: <br /> <input type="text" name="login" /> <br />
+	<div class="container">
+	<div class="vertical-al">
+		<h3> Zaloguj się </h3>
+		<p>
+		Login: <br /> <input type="text" name="login" /> <br /> 
 		Hasło: <br /> <input type="password" name="password" /> <br /><br />
 		<input type="submit" value="Zaloguj się" />
-	
+		</p>
+		</div>
+	</div>
 	</form>
 
 	<?php

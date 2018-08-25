3c084b96dd415c00bc0c3c8aee20bea57d172832
Piotr Pawlaczyk <piotrpawlaczyk21@gmail.com>
2018-08-25 13:10:59 +0200

    New style index.php

--- PHP_scripts/index.php
+++ PHP_scripts/index.php
@@ -22,23 +22,38 @@
 <html>
 <head>
 <title>PANEL LOGOWANIA</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link rel="stylesheet" type="text/css" href="index_style.css" title="Arkusz stylów CSS">
+
+
+
+
 </head>
 <body>
 
 	<form action="login.php" method="post">
-	
-		Login: <br /> <input type="text" name="login" /> <br />
+	<div class="container">
+	<div class="vertical-al">
+		<h3> Zaloguj się </h3>
+		<p>
+		Login: <br /> <input type="text" name="login" /> <br /> 
 		Hasło: <br /> <input type="password" name="password" /> <br /><br />
 		<input type="submit" value="Zaloguj się" />
-	
-	</form>
-	
-	
-	<?php
+		</p>
+		<?php
 	if(isset($_SESSION['error'])) 
 	{
 		echo $_SESSION['error'];
 	}
 	?>
+		</div>
+	
+	
+	</div>
+	
+	</form>
+	
+	
+	
 </body>
 </html>
